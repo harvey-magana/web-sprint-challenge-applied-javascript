7d468799f2e3351e93d2f38fa926363dceecfd46
harvey magana <heyharv2012@gmail.com>
2020-07-17 18:25:48 -0700

    Created the code for the first component.

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,24 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+    let headerContainer = document.querySelector('.header-container');
+    let header = document.createElement('div');
+    let date = document.createElement('span');
+    let title = document.createElement('h1');
+    let weather = document.createElement('span');
+
+    header.classList.add('header');
+    date.classList.add('date');
+    date.textContent = 'MARCH 28, 2020';
+    title.textContent = 'Lambda Times';
+    weather.classList.add('temp');
+    weather.textContent = '98°';
+
+    header.appendChild(date);
+    header.appendChild(title);
+    header.appendChild(weather);
+    headerContainer.appendChild(header);
+
+    return headerContainer;
+}
