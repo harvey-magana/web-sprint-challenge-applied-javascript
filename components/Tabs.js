66a2a7d8e17b17db74513fd8df0ed22a322a2890
harvey magana <heyharv2012@gmail.com>
2020-07-19 12:17:55 -0700

    Getting the api call to add the topics to the tabs.

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,26 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+
+
+
+
+let entry = document.querySelector('.topics');
+
+axios.get("https://lambda-times-backend.herokuapp.com/topics")
+.then((successResponse) => {
+  //console.log(successResponse.data.topics, "the data");
+  Object.entries(successResponse.data.topics).forEach( (key) => {
+      console.log(key);
+    let newTopic = document.createElement('div');
+    newTopic.classList.add('tab');
+    newTopic.textContent = key;
+    //newTopic.appendChild(key)
+    console.log(newTopic)
+    entry.appendChild(newTopic);
+  })
+})
+.catch((errorResponse) => {
+  console.log('error!', errorResponse);
+})
